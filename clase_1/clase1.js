c7dc0583942a6554822c908176fe2cb3d5707580
obduber <obduber901@@gmail.com>
2020-03-04 13:07:30 -0400

    Clase de 04/03/20

--- clase_1/clase1.js
+++ clase_1/clase1.js
@@ -11,7 +11,8 @@ alert("El ancho y largo de la pantalla es:" +ancho+","+largo);
 /*function imprimir(){
 	window.print();
 }*/
-alert(navigator.appName);
+
+/*alert(navigator.appName);
 alert(navigator.appVersion);
 alert(navigator.product);
 alert(navigator.userAgent);
@@ -19,3 +20,20 @@ alert(navigator.platform);
 alert(navigator.language);
 alert(navigator.online);
 alert(navigator.cokieEnabled);
+*/
+
+function maximizar(){ 
+
+ventana=window.open("cerrar.html","",screen.width,screen.height);
+ajustar();
+}
+
+function ajustar(){
+
+	ventana.moveTo(0,0);
+ventana.resizeTo(screen.width,screen.height);
+}
+
+function cerrar(){
+	window.close()
+}
