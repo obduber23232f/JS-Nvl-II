2a6d9fc17dce33024bf3de15acec83513194962b
obduber <obduber901@@gmail.com>
2020-03-04 12:29:19 -0400

    Clase de 04/03/20

--- clase_1/clase1.js
+++ clase_1/clase1.js
@@ -1,2 +1,21 @@
-var url;
-url=prompt("Ingrese el URL");
+/*var url;
+window.location=url=prompt("Ingrese el URL");
+
+*/
+
+/*var ancho= screen.width;
+var largo = screen.height;
+
+alert("El ancho y largo de la pantalla es:" +ancho+","+largo);
+*/
+/*function imprimir(){
+	window.print();
+}*/
+alert(navigator.appName);
+alert(navigator.appVersion);
+alert(navigator.product);
+alert(navigator.userAgent);
+alert(navigator.platform);
+alert(navigator.language);
+alert(navigator.online);
+alert(navigator.cokieEnabled);
